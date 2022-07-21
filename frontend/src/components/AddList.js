a5b7ab00d0337091c2efaf851e9b8a6cd1941fbd
YavorBorissov <yavor.borissov@gmail.com>
2022-07-21 10:31:52 +0300

    AddList component

---
+++ frontend/src/components/AddList.js
@@ -0,0 +1,92 @@
+import React, { useCallback } from 'react'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+import MenuItem from '@mui/material/MenuItem'
+import Icon from '@mui/material/Icon'
+import { Box } from '@mui/system'
+
+function AddList() {
+  const [open, setOpen] = React.useState(false)
+  const [type, setType] = React.useState('Birthday')
+
+  const listTypes = [
+    'Birthday',
+    'Wedding',
+    'Graduation/Prom',
+    'Christmas',
+    'Other'
+  ]
+
+  const handleClickOpen = useCallback(() => {
+    setOpen(true)
+  }, [])
+
+  const handleClose = useCallback(() => {
+    setOpen(false)
+  }, [])
+
+  const handleChange = useCallback(event => {
+    setType(event.target.value)
+  }, [])
+
+  const onSubmit = useCallback(async e => {
+    e.preventDefault()
+  }, [])
+
+  return (
+    <div>
+      <Button
+        sx={{ mt: 3.5 }}
+        variant="outlined"
+        onClick={handleClickOpen}
+        startIcon={<Icon color="primary">add_circle</Icon>}
+      >
+        CREATE NEW LIST
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <Box component="form" onSubmit={onSubmit}>
+          <DialogTitle>Your new list</DialogTitle>
+          <DialogContent>
+            <TextField
+              select
+              margin="normal"
+              id="name"
+              label="List type"
+              value={type}
+              onChange={handleChange}
+              fullWidth
+              variant="outlined"
+            >
+              {listTypes.map(option => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              autoFocus
+              required
+              margin="normal"
+              id="name"
+              label="List name"
+              fullWidth
+              variant="outlined"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose} type="submit">
+              Create List
+            </Button>
+          </DialogActions>
+        </Box>
+      </Dialog>
+    </div>
+  )
+}
+
+export default AddList
