8b07240f780a8996caf350be23788e76372bf566
Emanuil Gospodinov <gospodinove@gmail.com>
2022-07-26 13:41:33 +0300

    Fetch list items

--- backend/routes/listsRoutes.js
+++ backend/routes/listsRoutes.js
@@ -5,6 +5,7 @@ const { extend } = require('indicative/validator')
 const { replaceId, sendErrorResponse } = require('../utils')
 const { passwordValidator, validationMessages } = require('../validation')
 const isAuthenticated = require('../middleware/isAuthenticated')
+const { ObjectId } = require('mongodb')
 
 const router = express.Router()
 
@@ -35,6 +36,33 @@ router.post('/', isAuthenticated, async (req, res) => {
     }
   } catch (errors) {
     sendErrorResponse(res, 500, 'field-error', errors)
+  }
+})
+
+router.get('/:id/items', isAuthenticated, async (req, res) => {
+  const db = req.app.locals.db
+
+  try {
+    const list = await db.collection('lists').findOne({
+      _id: ObjectId(req.params.id),
+      users: req.session.user.id
+    })
+
+    replaceId(list)
+
+    if (!list) {
+      sendErrorResponse(res, 404, 'general', 'Could not find your list')
+      return
+    }
+
+    const items = await db
+      .collection('listItems')
+      .find({ listId: list.id.toString() })
+      .toArray()
+
+    res.json({ success: true, items: items.map(item => replaceId(item)) })
+  } catch (errors) {
+    sendErrorResponse(res, 500, 'general', 'Could not fetch your list items')
   }
 })
 
