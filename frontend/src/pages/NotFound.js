28a31870c85b89b5c25b7d2cbe599e7853d064bc
Emanuil Gospodinov <gospodinove@gmail.com>
2022-08-23 12:36:22 +0300

    [#63] - 404 page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,7 +1,17 @@
+import { Box } from '@mui/material'
 import { memo } from 'react'
+import notFoundGif from '../assets/404.gif'
 
-function NotFound() {
-  return <h1>404</h1>
-}
+const NotFound = () => (
+  <Box
+    width="100%"
+    height="100%"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <img src={notFoundGif} alt="Loading..." width="40%" />
+  </Box>
+)
 
 export default memo(NotFound)
