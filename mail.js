f9f57dcbd86b7090f278599de7b68810b940057c
Emanuil Gospodinov <gospodinove@gmail.com>
2022-09-30 13:33:21 +0300

    Switch to Gmail

--- mail.js
+++ mail.js
@@ -5,7 +5,9 @@ const { inviteTemplate } = require('./mailTemplates/invite')
 dotenv.config()
 
 const transporter = nodemailer.createTransport({
-  service: 'hotmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD
